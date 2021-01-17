84748e2fb91829d75a15182e62d10b76dcde6b82
36725950 <36725950+Ray-Hackshaw@users.noreply.github.com>
2021-01-17 20:56:49 +1300

    Changing navigation tabs and centering education page.

--- js/edu.js
+++ js/edu.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     setTimeout(function() {
         $.getJSON('./assets/data.json', function(data) {
             $.each(data.courses, function(_, e) {
-                $('body').append('<p>' + e.code + ' - ' + e.title + '</p>' + '<p>' + e.description + '</p>' + '<p>' + e.projects + '</p>');
+                $('body').append('<p style="text-align: center;">' + e.code + ' | ' + e.title + '</p>' + '<p style="margin: 0 30%;">' + e.description + '</p>' + '<p>' + e.projects + '</p>');
             })
     }, 40000)
     })
